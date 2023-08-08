915e17f1310b0cee3093558495347990165a53b7
ErgonomicQ <ergonomicQ@gmail.com>
2023-08-07 20:57:53 -0700

    created User.Js, added to index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Animals = require('./Animals');
 const Cat = require('./Cat');
 const Dog = require('./Dog');
+const User = require('./User')
 
 // Set up relationships between models
 Cat.belongsTo(Animals);
@@ -10,4 +11,5 @@ module.exports = {
   Animals,
   Cat,
   Dog,
+  User
 };
