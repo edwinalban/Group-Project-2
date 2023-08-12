d987a498943f221467456712d0ccf20d3469de79
Jen Sandage <jensandage1@gmail.com>
2023-08-11 22:48:38 -0700

    got images to display on allanimals

--- seeds/animalsData.js
+++ seeds/animalsData.js
@@ -3,39 +3,48 @@ const { Animals } = require('../models');
 const animalsData = [
     {
         name: "Willow",
-        age: 2
+        age: 2,
+        filename: "cat1.jpg"
     },
     {
         name: "Whomping",
-        age: 1
+        age: 1,
+        filename: "cat2.jpg"
     },
     {
         name: "Wedge",
-        age: 6
+        age: 6,
+        filename: "cat3.jpg"
     },
     {
         name: "Walter",
-        age: 6
+        age: 6,
+        filename: "cat4.jpg"
     },
     {
         name: "Winston",
-        age: 7
+        age: 7,
+        filename: "cat5.jpg"
     },
     {
         name: "Sir Podrick",
-        age: 2
+        age: 2,
+        filename: "dog1.jpg"
     },
     {
         name: "Sisu",
-        age: 8
+        age: 8,
+        filename: "dog2.jpg"
     },
     {
         name: "Sky",
-        age: 8
+        age: 8,
+        filename: "dog3.jpg"
     },
     {
         name: "Anya",
-        age: 6
+        age: 6,
+        filename: "dog4.jpg"
     }
 ];
 
