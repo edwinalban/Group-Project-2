e7dddd0fe8dedd689d4f4d1a5eeed049c552c728
nolli <nolliepolieollie@gmail.com>
2023-08-16 19:23:58 -0700

    moving things into place

--- controllers/votingRoutes.js
+++ controllers/votingRoutes.js
@@ -23,6 +23,7 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //eventlistener for vote
 router.post('/vote', (req, res) => {
   const selectedItemId = req.body.id; //assuming itemID
@@ -31,42 +32,6 @@ router.post('/vote', (req, res) => {
   res.json({ message: "Vote submitted" });
 });
 
-function castVote(id)
-fetch('/vote', {
-  method: 'POST',
-  body: JSON.stringify({ id })
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('failed to submit Vote')
-    }
-  return response.json();
-  })
-  .then(data => {
-    //handle response from server e.g. update UI 
-  })
-console.log('Vote cast for item with ID ${itemID}');
-
-// .catch (error => {
-//   console.error('Error:', error);
-// });
-
-document.querySelectorAll(".vote-button").forEach(button => {
-  button.addEventListener("click", function () {
-    const id = selectedItemId;
-    castVote(selectedItemIdid);
-  });
-
-});
-
-
-
-//eventListener for comment submission
-document.getElementById("comment-submit-btn").addEventListener("click", function (event) {
-  event.preventDefault();
-  const userInput = document.getElementById("user-input").ariaValueMax;
-  submitComment(userInput);
-});
 
 
 
